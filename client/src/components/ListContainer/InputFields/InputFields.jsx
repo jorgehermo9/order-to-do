981aaa9fd4957862d33027608550cc27f3e9a89c
Jorge Hermo Gonzalez <jorge.hermo.gonzalez@udc.es>
2021-07-09 01:53:21 +0200

    update

--- client/src/components/ListContainer/InputFields/InputFields.jsx
+++ client/src/components/ListContainer/InputFields/InputFields.jsx
@@ -3,6 +3,8 @@ import AddCircleRoundedIcon from '@material-ui/icons/AddCircleRounded';
 import "./InputFields.css"
 function InputFields(props){
 	function handleSubmmit(){
+		if(itemDesc.length ===0) return;
+
 		let item = {desc: itemDesc};
 		props.appendToList(item);
 		setItemDesc("");
@@ -12,6 +14,15 @@ function InputFields(props){
 		<div className="input-container">
 			<input className = "input-item" type="text" onChange={e => setItemDesc(e.target.value)} value={itemDesc}/>
 			<AddCircleRoundedIcon className="submmit" onClick={handleSubmmit}/>
+			<div>
+				<label for="pngInput">Choose png</label>
+				<input className="input-file" type="file" accept=".jpg .jpeg .png" name="pngInput" id="pngInput"/>
+			</div>
+			<div>
+				<label for="svgInput">Choose svg</label>
+				<input className="input-file" type="file" accept=".svg" name="svgInput" id="svgInput"/>
+			</div>
+
 		</div>
 	)
 }
