04708671689e1bca46c3bc3e3d5aaabe0e36fbbe
Jorge Hermo Gonzalez <jorge.hermo.gonzalez@udc.es>
2021-07-08 12:43:22 +0200

    update

---
+++ client/src/components/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ListContainer from "./ListContainer/ListContainer";
+
+
+function App(){
+	return(<ListContainer/>);
+}
+
+export default App;
---
+++ client/src/components/ListContainer/ListContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./ListContainer.css"
+
+function ListContainer(props) {
+	return(<div className="main-container">
+
+	</div>)
+}
+
+export default ListContainer;
