c8a7dd2d943e286a1531c0bdce8c354b83d2160a
jorgehermo9 <jorgehermogonzalez@gmail.com>
2021-07-27 13:36:03 +0200

    Fixed payload limit to 50mb

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const api = require("./routes/api");
 
 const app = express();
 app.use(cors());
-app.use(express.json())
+app.use(express.json({limit:"50mb"}))
 app.use(express.urlencoded({extended:true}));
 
 
